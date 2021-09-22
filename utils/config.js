7096a5ebccd0b3b793bcd57bc6241325d6bb6525
ombayus <>
2021-09-22 21:56:10 +0300

    Use github secrets for config

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 4000
-const MONGODB_URI = process.env.MONGODB_URI
-const SESSION_SECRET = process.env.SESSION_SECRET
-const EMAIL_NAME = process.env.EMAIL_NAME
-const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+const PORT = (process.env.PORT || secrets.PORT)|| 4000
+const MONGODB_URI = process.env.MONGODB_URI || secrets.MONGODB_URI
+const SESSION_SECRET = process.env.SESSION_SECRET || secrets.SESSION_SECRET
+const EMAIL_NAME = process.env.EMAIL_NAME || secrets.EMAIL_NAME
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD || secrets.EMAIL_PASSWORD
 
 
 module.exports = {
