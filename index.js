b45ba2d489664e879390fd6b87b9d6c5a0217ef9
Omer Bayramcavus <47576905+OmBayus@users.noreply.github.com>
2021-09-24 00:26:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(session({
 }));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000','https://www.ombayus.com/'],
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
