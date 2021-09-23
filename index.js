8cef72f69db79e48a8274bf7ad5b61159cecf3f4
Omer Bayramcavus <47576905+OmBayus@users.noreply.github.com>
2021-09-23 23:46:41 +0300

    bosluk eklendi

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 80
 
 server.listen(PORT, () => {
-  console.log('listening the server on '+PORT);
+  console.log('listening the server on ' + PORT);
 });
 
 
