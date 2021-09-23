9063a5e6c214d9e87a48774f45ae714a5238708a
Omer Bayramcavus <47576905+OmBayus@users.noreply.github.com>
2021-09-24 00:12:04 +0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI || secrets.MONGODB_URI
-const SESSION_SECRET = process.env.SESSION_SECRET || secrets.SESSION_SECRET
-const EMAIL_NAME = process.env.EMAIL_NAME || secrets.EMAIL_NAME
-const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD || secrets.EMAIL_PASSWORD
+const MONGODB_URI = process.env.MONGODB_URI
+const SESSION_SECRET = process.env.SESSION_SECRET
+const EMAIL_NAME = process.env.EMAIL_NAME
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
 
 
 module.exports = {
