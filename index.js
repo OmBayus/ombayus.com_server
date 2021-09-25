e44343bb53d3227483d354242e1b021312587506
ombayus <>
2021-09-25 11:41:38 +0300

    Unitialized

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(session({
   secret: SESSION_SECRET,
   resave: true,
   proxy:true,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: { secure: true, maxAge:  6*60*60*1000,sameSite:'none'},
   unset: 'destroy'
 }));
