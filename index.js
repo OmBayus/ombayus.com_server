609a55d6e7362e8f7ca1321a3c42dd4e622064c6
ombayus <>
2021-09-29 23:36:54 +0300

    Test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(session({
   resave: false,
   proxy:true,
   saveUninitialized: true,
-  cookie: { secure: true, maxAge:  6*60*60*1000,sameSite:'none'},
+  cookie: { secure: true, maxAge:  6*60*60*1000,sameSite:true},
   unset: 'destroy'
 }));
 
