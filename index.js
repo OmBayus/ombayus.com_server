313d3c879a82ffe9de47b301304d3a1f4fcc6a7b
ombayus <>
2021-10-01 09:16:25 +0300

    Test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(session({
     secure: process.env.NODE_ENV === "production",
     httpOnly:true, 
     maxAge:  6*60*60*1000,
-    sameSite:process.env.NODE_ENV === "production" ? 'none' : 'lax',
+    sameSite:process.env.NODE_ENV === "production" ? 'lax' : 'lax',
     signed:true,
   },
   unset: 'keep'
