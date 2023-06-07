d6027311a97bbcb98b9ad11370d8530527c3b26b
DESKTOP-PRCV026\omerb <omerbayramcavus@gmail.com>
2023-06-07 23:32:08 +0300

    contact error handler

--- routers/contact.js
+++ routers/contact.js
@@ -1,89 +1,85 @@
-const router= require('express').Router()
-const Contact = require("../models/contact")
-const SendMail = require("../utils/sendEmail")
-const {authExactor} = require("../utils/middleware")
+const router = require("express").Router();
+const Contact = require("../models/contact");
+const SendMail = require("../utils/sendEmail");
+const { authExactor } = require("../utils/middleware");
 
-router.post("/sendMsg",async(req,res)=>{
-
-    if(req.body.msg){
-        return res.json({error:"You can send one message per hour."})
+router.post("/sendMsg", async (req, res) => {
+  try {
+    if (req.body.msg) {
+      return res.json({ error: "You can send one message per hour." });
     }
-    
+
     const newMsg = new Contact({
-        name:req.body.name,
-        email:req.body.email,
-        message:req.body.message
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+    });
+
+    newMsg.save((err) => {
+      if (err) {
+        return res.json({ error: "error" });
+      }
+      SendMail(
+        "omerbayramcavus@gmail.com",
+        "ombayus.com Contact Message",
+        newMsg
+      );
+      res.json(newMsg);
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.use(authExactor);
+
+router.get("/getAll", (req, res) => {
+  Contact.find({}, (err, items) => {
+    if (!err) {
+      res.json(items);
+    }
+  });
+});
+
+router.get("/getLength", async (req, res) => {
+  var len = await Contact.count();
+  res.json({ len });
+});
+
+router.get("/getWithPagination", async (req, res) => {
+  var len = await Contact.count();
+
+  const resultsPerPage = 5;
+  let page = req.query.page >= 1 ? req.query.page : 1;
+
+  page = page - 1;
+
+  Contact.find({})
+    .sort({ $natural: -1 })
+    .limit(resultsPerPage)
+    .skip(resultsPerPage * page)
+    .then((results) => {
+      return res.status(200).json({
+        len,
+        page: page + 1,
+        results,
+      });
     })
+    .catch((err) => {
+      return res.json([]);
+    });
+});
 
-    newMsg.save((err)=>{
-        if(err){
-            return res.json({error:"error"})
-        }
-        SendMail("omerbayramcavus@gmail.com","ombayus.com Contact Message",newMsg)
-        res.json(newMsg)
-
+router.post("/del", async (req, res) => {
+  Contact.findOneAndDelete({ _id: req.body._id })
+    .then((item) => {
+      if (!item) {
+        res.json({ error: "The project has already been deleted." });
+        return;
+      }
+      res.json(item);
     })
-    
+    .catch((err) => res.json({ error: err.message }));
+});
 
-
-})
-
-router.use(authExactor)
-
-router.get("/getAll",(req,res)=>{
-    Contact.find({},(err,items)=>{
-        if(!err){
-              res.json(items)
-        }
-    })
-})
-
-router.get("/getLength",async(req,res)=>{
-    var len = await Contact.count();
-    res.json({len})
-})
-
-router.get("/getWithPagination",async(req,res)=>{
-    var len = await Contact.count();
-
-    const resultsPerPage = 5;
-    let page = req.query.page >= 1 ? req.query.page : 1;
-
-    page = page - 1
-
-    Contact.find({})
-        .sort({$natural:-1})
-        .limit(resultsPerPage)
-        .skip(resultsPerPage * page)
-        .then((results) => {
-            return res.status(200).json({
-                len,
-                page:(page+1),
-                results
-            });
-        })
-        .catch((err) => {
-            return res.json([]);
-        });
-
-    
-})
-
-router.post("/del",async(req,res)=>{
-
-    Contact.findOneAndDelete({_id:req.body._id})
-    .then(item=>{
-        if(!item){
-            res.json({error:"The project has already been deleted."})
-            return
-        }
-        res.json(item)
-    })
-    .catch(err=>res.json({error:err.message}))
-
-})
-
-
-
-
-module.exports = router
+module.exports = router;
--- routers/traffic.js
+++ routers/traffic.js
@@ -1,64 +1,74 @@
-const router= require('express').Router()
-const Traffic = require("../models/traffic")
-const geoip = require('geoip-lite');
-const {authExactor} = require("../utils/middleware")
+const router = require("express").Router();
+const Traffic = require("../models/traffic");
+const geoip = require("geoip-lite");
+const { authExactor } = require("../utils/middleware");
 
-
-router.post("/send",async(req,res)=>{
-    const origins = process.env.NODE_ENV !== "production" ? ['http://localhost:3000','http://localhost:3001','http://localhost:3002']: ['https://www.ombayus.com','https://admin.ombayus.com']
-    if(origins.includes(req.headers.origin)){
-        var ip = process.env.NODE_ENV !== "production" ? "1.1.1.1" : req.headers['x-forwarded-for'] || req.socket.remoteAddress
-        if(ip){
-            const trafficmod = await Traffic.findOne({name:"Traffic"})
-            const traffic = {
-                name:"Traffic",
-                country:trafficmod.country,
-                logins:trafficmod.logins,
-                lastLogins:trafficmod.lastLogins
-            }
-            var geo = geoip.lookup(ip);
-            if(!geo){
-                return res.json(false)
-            }
-            if(traffic.country[geo.country]){
-                traffic.country[geo.country] += 1
-            }
-            else{
-                traffic.country[geo.country] = 1
-            }
-    
-            if(traffic.lastLogins.length > 10){
-                traffic.lastLogins.shift()
-            }
-
-            traffic.lastLogins.push({ip:(ip+" ( " +geo.country+"/" +geo.city+ " )" ),date:new Date().toString()})
-    
-            if(traffic.logins[new Date().toLocaleDateString()]){
-                traffic.logins[new Date().toLocaleDateString()]+= 1
-            }
-            else{
-                traffic.logins[new Date().toLocaleDateString()] = 1
-            }
-
-            Traffic.findOneAndUpdate({name:"Traffic"},traffic,{new:true}).then(t=>{
-                
-            })
-            return res.json(true)
+router.post("/send", async (req, res) => {
+  try {
+    const origins =
+      process.env.NODE_ENV !== "production"
+        ? [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "http://localhost:3002",
+          ]
+        : ["https://www.ombayus.com", "https://admin.ombayus.com"];
+    if (origins.includes(req.headers.origin)) {
+      var ip =
+        process.env.NODE_ENV !== "production"
+          ? "1.1.1.1"
+          : req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+      if (ip) {
+        const trafficmod = await Traffic.findOne({ name: "Traffic" });
+        const traffic = {
+          name: "Traffic",
+          country: trafficmod.country,
+          logins: trafficmod.logins,
+          lastLogins: trafficmod.lastLogins,
+        };
+        var geo = geoip.lookup(ip);
+        if (!geo) {
+          return res.json(false);
         }
+        if (traffic.country[geo.country]) {
+          traffic.country[geo.country] += 1;
+        } else {
+          traffic.country[geo.country] = 1;
+        }
+
+        if (traffic.lastLogins.length > 10) {
+          traffic.lastLogins.shift();
+        }
+
+        traffic.lastLogins.push({
+          ip: ip + " ( " + geo.country + "/" + geo.city + " )",
+          date: new Date().toString(),
+        });
+
+        if (traffic.logins[new Date().toLocaleDateString()]) {
+          traffic.logins[new Date().toLocaleDateString()] += 1;
+        } else {
+          traffic.logins[new Date().toLocaleDateString()] = 1;
+        }
+
+        Traffic.findOneAndUpdate({ name: "Traffic" }, traffic, {
+          new: true,
+        }).then((t) => {});
+        return res.json(true);
+      }
     }
-    res.json(false)
-    
-})
+    res.json(false);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-router.use(authExactor)
+router.use(authExactor);
 
-router.get("/get",async(req,res)=>{
-    const trafficmod = await Traffic.findOne({name:"Traffic"})
-    res.json(trafficmod)
-})
-
-
-
+router.get("/get", async (req, res) => {
+  const trafficmod = await Traffic.findOne({ name: "Traffic" });
+  res.json(trafficmod);
+});
 
 // router.post("/create",(req,res)=>{
 //     const traffic = new Traffic({
@@ -70,4 +80,4 @@ router.get("/get",async(req,res)=>{
 //     traffic.save().then((data)=>res.json(data))
 // })
 
-module.exports = router
+module.exports = router;
